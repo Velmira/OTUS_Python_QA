1a762a6384e48468198f1f739d21fb0fd53455bd
Velmira <ve1mira@mail.ru>
2023-09-07 22:01:53 +0300

    add fix_me.py and .gitignore

---
+++ fix_me.py
@@ -0,0 +1,9 @@
+def calculate_average(nums):
+    total = sum(nums)
+    count = len(nums)
+    average = total / count
+    print("The average is:", average)
+
+
+numbers = [10, 15, 20]
+calculate_average(numbers)
